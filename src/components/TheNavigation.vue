43fb3852cee4bf8bbc91492738e1b02c5e348616
Joseph Hammad <joseph.hammad@epitech.eu>
2020-03-27 23:07:26 +0100

    init vuetify + navbar component

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar app color="white" elevate-on-scroll>
+    <v-toolbar-title>
+      <div class="title"><span>&#128054;</span>Doggos as a service</div>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text>
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<style lang="scss" scoped>
+.title {
+  span {
+    font-size: 25px;
+    margin-right: 10px;
+  }
+}
+</style>
